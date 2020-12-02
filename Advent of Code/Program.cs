3106e03aab5122a72c1e31568078c8fdffee7f48
Jessica Krishtul <jessica.krishtul@carlisletechnology.com>
2020-12-01 20:42:38 -0500

    Add project files.

---
+++ Advent of Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advent_of_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
